b15f96373135b6e781d07303ba2f1436f09cc6fd
EkimovaN <Ekimova.n.u@mail.ru>
2024-12-29 20:34:57 +0500

    Add files via upload

---
+++ peremennaja.ру.py
@@ -0,0 +1,13 @@
+tasks_donte=12
+hours_spent=1.5
+course_name="Python"
+time_per_task=(hours_spent/tasks_donte)
+print(f'Курс:{course_name},Всего задач:{tasks_donte},затрачено часов:{hours_spent},среднее время выполнения {time_per_task} часа.')
+
+
+first_name = 'Vasya'
+apple_count = 10
+second_name = 'Petya'
+orange_count = 15
+print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+
